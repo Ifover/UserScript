c4d488d4a4512f31b3b3d727bcf2cee8eb90ac07
Ifover <Ifover@outlook.com>
2024-12-17 23:46:31 +0800

    MC百科 - 便捷工具 v0.2

--- mcmod-tools.user.js
+++ mcmod-tools.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         MC百科 - 便捷工具
 // @namespace    https://github.com/ifover/UserScript
-// @version      0.1
+// @version      0.2
 // @author       ifover
 // @description  在MC百科首页显示收藏列表，方便导航
 // @license      GPL-3.0 License
 // @icon         https://www.mcmod.cn/images/favicon.ico
 // @match        https://*.mcmod.cn/
+// @require      https://kit.fontawesome.com/d4dda3d6cc.js
 // @require      https://cdn.jsdelivr.net/npm/vue@3.5.13/dist/vue.global.prod.js
 // @require      https://unpkg.com/vue-demi@latest/lib/index.iife.js
 // @require      data:application/javascript,%3Bwindow.Vue%3DVue%3B
@@ -24,7 +25,7 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-(e=>{if(typeof GM_addStyle=="function"){GM_addStyle(e);return}const t=document.createElement("style");t.textContent=e,document.head.append(t)})(" #app{background-color:#e9e9e9;font-size:12px;position:fixed;left:10px;top:286px;border-radius:4px;z-index:10}.ant-tree{background-color:transparent}.ant-tree-title a{overflow:hidden;white-space:nowrap;text-overflow:ellipsis;width:230px}.ant-tree-treenode.ant-tree-treenode-switcher-close,.ant-tree-treenode.ant-tree-treenode-switcher-open,.ant-tree-treenode.ant-tree-treenode-disabled{position:sticky;top:0;z-index:10;background-color:#e9e9e9}.ant-tree-treenode.ant-tree-treenode-disabled{top:28px}#app[data-v-8268fb43]{background-color:#e9e9e9;font-size:12px;position:fixed;left:10px;top:286px;border-radius:4px;z-index:10}.ant-tree[data-v-8268fb43]{background-color:transparent}.ant-tree-title a[data-v-8268fb43]{overflow:hidden;white-space:nowrap;text-overflow:ellipsis;width:230px}.ant-tree-treenode.ant-tree-treenode-switcher-close[data-v-8268fb43],.ant-tree-treenode.ant-tree-treenode-switcher-open[data-v-8268fb43],.ant-tree-treenode.ant-tree-treenode-disabled[data-v-8268fb43]{position:sticky;top:0;z-index:10;background-color:#e9e9e9}.ant-tree-treenode.ant-tree-treenode-disabled[data-v-8268fb43]{top:28px}.mm_tools[data-v-8268fb43]{width:362px;padding:6px 8px;max-height:520px;overflow-y:scroll}.mm_tools .mm_mask[data-v-8268fb43]{width:calc(100% - 8px);height:6px;position:absolute;left:0;right:0;top:0;z-index:11;border-radius:4px 4px 0 0;background-color:#e9e9e9}.mm_tools[data-v-8268fb43]::-webkit-scrollbar{width:8px}.mm_tools[data-v-8268fb43]::-webkit-scrollbar-thumb{border-radius:8px;background-color:#c1c1c1}.mm_tools[data-v-8268fb43]::-webkit-scrollbar-track{background-color:#f1f1f1;border-radius:0 8px 8px 0}.cover[data-v-8268fb43]{width:60px;opacity:.77;margin-right:6px}.mm_fav_li[data-v-8268fb43]{display:flex;align-items:center;padding:2px 0}.mm_fav_title[data-v-8268fb43]{width:230px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis} ");
+(e=>{if(typeof GM_addStyle=="function"){GM_addStyle(e);return}const t=document.createElement("style");t.textContent=e,document.head.append(t)})(" #app,#app[data-v-22dd0357]{width:333px;max-height:520px;cursor:default;background-color:#e9e9e9;font-size:12px;position:fixed;left:10px;top:286px;border-radius:4PX 4px 0 0;z-index:10}.mm_sidebar[data-v-22dd0357]{cursor:pointer;position:fixed;left:-24px}.mm_sidebar[data-v-22dd0357]:hover{left:0;transition:.3s ease-out}.mm_sidebar .mm_sidebar_fav[data-v-22dd0357]{box-sizing:content-box;width:26px;height:26px;font-size:26px;padding:8px;color:#ccc}.mm_header[data-v-22dd0357]{height:24px;border-radius:4px 4px 0 0;background-color:#ccc;padding:0 5px;text-align:right}.mm_header .mm_close[data-v-22dd0357]{cursor:pointer;color:#191919}.mm_content[data-v-22dd0357]{max-height:484px;overflow-y:scroll;padding:6px 8px}.mm_content .mm_fav_li[data-v-22dd0357]{display:flex;align-items:center;padding:2px 0;cursor:pointer}.mm_content .mm_fav_li .mm_fav_cover[data-v-22dd0357]{width:60px;opacity:.77;margin-right:6px}.mm_content .mm_fav_li .mm_fav_title[data-v-22dd0357]{width:182px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis}[data-v-22dd0357]::-webkit-scrollbar{width:8px}[data-v-22dd0357]::-webkit-scrollbar-thumb{background-color:#c1c1c1}[data-v-22dd0357]::-webkit-scrollbar-track{background-color:#f1f1f1}[data-v-22dd0357] .mm_tree_lv2 .ant-tree-switcher.ant-tree-switcher-noop{width:12px}[data-v-22dd0357] .mm_tree_lv2 .ant-tree-title a{text-decoration:none;cursor:pointer}[data-v-22dd0357] .mm_tree_lv2 .ant-tree-title a:hover *{color:#2575f9}[data-v-22dd0357] .ant-tree{background-color:transparent}[data-v-22dd0357] .ant-tree .ant-tree-title{display:flex;align-items:center}[data-v-22dd0357] .ant-tree .ant-tree-title a{overflow:hidden;white-space:nowrap;text-overflow:ellipsis}[data-v-22dd0357] .ant-tree .ant-tree-title .mm_title_icon{font-size:12px;margin-left:5px;color:#888}[data-v-22dd0357] .ant-tree .ant-tree-treenode.ant-tree-treenode-switcher-close,[data-v-22dd0357] .ant-tree .ant-tree-treenode.ant-tree-treenode-switcher-open,[data-v-22dd0357] .ant-tree .ant-tree-treenode.ant-tree-treenode-disabled{position:sticky;top:-6px;z-index:10;background-color:#e9e9e9}[data-v-22dd0357] .ant-tree .ant-tree-treenode.ant-tree-treenode-disabled{top:22px} ");
 
 (function (vue, pinia$1, antDesignVue) {
   'use strict';
@@ -67,17 +68,50 @@
       });
     });
   };
-  const _hoisted_1 = { class: "mm_tools" };
-  const _hoisted_2 = { class: "mm_fav_list" };
-  const _hoisted_3 = ["href", "title"];
-  const _hoisted_4 = { class: "mm_fav_li" };
-  const _hoisted_5 = ["src", "alt"];
-  const _hoisted_6 = { class: "mm_fav_title" };
+  const _hoisted_1 = {
+    key: 0,
+    class: "mm_sidebar"
+  };
+  const _hoisted_2 = {
+    key: 1,
+    class: "mm_header"
+  };
+  const _hoisted_3 = {
+    key: 2,
+    class: "mm_content"
+  };
+  const _hoisted_4 = {
+    key: 0,
+    class: "fas fa-folder-open"
+  };
+  const _hoisted_5 = {
+    key: 1,
+    class: "fas fa-folder"
+  };
+  const _hoisted_6 = ["href", "title"];
+  const _hoisted_7 = { class: "mm_fav_li" };
+  const _hoisted_8 = ["src", "alt"];
+  const _hoisted_9 = { class: "mm_fav_title" };
+  const _hoisted_10 = {
+    key: 0,
+    title: "模组",
+    class: "mm_title_icon"
+  };
+  const _hoisted_11 = {
+    key: 1,
+    title: "整合包",
+    class: "mm_title_icon"
+  };
+  const _hoisted_12 = {
+    key: 2,
+    class: "mm_title_icon"
+  };
   const _sfc_main = /* @__PURE__ */ vue.defineComponent({
     __name: "App",
     setup(__props) {
       const userStore = useUserStore();
       vue.reactive([]);
+      let modalShow = vue.ref(true);
       const expandedKeys = vue.ref([]);
       const selectedKeys = vue.ref([]);
       let treeFavData = vue.ref([]);
@@ -103,7 +137,7 @@
         let node = new DOMParser().parseFromString(html, "text/html");
         let nodeList = node.querySelectorAll(".favorite-fold-list ul > a");
         let _favList = [];
-        nodeList.forEach((n) => {
+        nodeList.forEach((n, i) => {
           var _a, _b;
           let favID = n.getAttribute("data-id");
           let favName = (_a = n.querySelector('li span[class="title"]')) == null ? void 0 : _a.getAttribute("title");
@@ -124,8 +158,8 @@
             title: favName,
             // title: `${favName} 模组x${modCount} 整合包x${modPackCount}`,
             modCount,
-            modPackCount,
-            isLeaf: modCount === 0 && modPackCount === 0
+            modPackCount
+            // isLeaf: modCount === 0 && modPackCount === 0,
           });
         });
         treeFavData.value = _favList;
@@ -165,6 +199,7 @@
                 key: modID,
                 modPic,
                 modURL,
+                class: "mm_tree_lv2",
                 isLeaf: true
               });
             });
@@ -173,6 +208,7 @@
             modArr.push({
               title: `模组 (${modCount})`,
               disabled: true,
+              class: "mm_tree_lv2",
               isLeaf: true
             });
             await getFavList("class");
@@ -181,9 +217,18 @@
             modArr.push({
               title: `整合包 (${modPackCount})`,
               disabled: true,
+              class: "mm_tree_lv2",
               isLeaf: true
             });
             await getFavList("modpack");
+          }
+          if (modCount === 0 && modPackCount === 0) {
+            modArr.push({
+              title: `这个收藏夹是空的。`,
+              disabled: true,
+              class: "mm_tree_lv2",
+              isLeaf: true
+            });
           }
           if (treeNode.dataRef) treeNode.dataRef.children = modArr;
           resolve();
@@ -195,19 +240,37 @@
         getFavoriteFold();
       });
       const handleTreeSelect = (sKeys, info) => {
-        console.log(sKeys);
         expandedKeys.value = sKeys;
       };
       return (_ctx, _cache) => {
+        const _component_a_button = vue.resolveComponent("a-button");
         const _component_a_tree = vue.resolveComponent("a-tree");
-        return vue.openBlock(), vue.createElementBlock("div", _hoisted_1, [
-          _cache[2] || (_cache[2] = vue.createElementVNode("div", { class: "mm_mask" }, null, -1)),
-          vue.createElementVNode("div", _hoisted_2, [
+        return vue.openBlock(), vue.createElementBlock(vue.Fragment, null, [
+          !vue.unref(modalShow) ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_1, [
+            vue.createElementVNode("i", {
+              class: "fa-solid fa-star mm_sidebar_fav",
+              onClick: _cache[0] || (_cache[0] = ($event) => vue.isRef(modalShow) ? modalShow.value = true : modalShow = true)
+            })
+          ])) : vue.createCommentVNode("", true),
+          vue.unref(modalShow) ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_2, [
+            vue.createVNode(_component_a_button, {
+              class: "mm_close",
+              type: "link",
+              size: "small",
+              onClick: _cache[1] || (_cache[1] = ($event) => vue.isRef(modalShow) ? modalShow.value = false : modalShow = false)
+            }, {
+              default: vue.withCtx(() => _cache[4] || (_cache[4] = [
+                vue.createElementVNode("i", { class: "fa-solid fa-xmark" }, null, -1)
+              ])),
+              _: 1
+            })
+          ])) : vue.createCommentVNode("", true),
+          vue.unref(modalShow) ? (vue.openBlock(), vue.createElementBlock("div", _hoisted_3, [
             vue.createVNode(_component_a_tree, {
               expandedKeys: expandedKeys.value,
-              "onUpdate:expandedKeys": _cache[0] || (_cache[0] = ($event) => expandedKeys.value = $event),
+              "onUpdate:expandedKeys": _cache[2] || (_cache[2] = ($event) => expandedKeys.value = $event),
               selectedKeys: selectedKeys.value,
-              "onUpdate:selectedKeys": _cache[1] || (_cache[1] = ($event) => selectedKeys.value = $event),
+              "onUpdate:selectedKeys": _cache[3] || (_cache[3] = ($event) => selectedKeys.value = $event),
               blockNode: true,
               "show-line": false,
               "show-icon": false,
@@ -216,30 +279,42 @@
               "load-data": onLoadData,
               onSelect: handleTreeSelect
             }, {
-              title: vue.withCtx(({ title, key, modPic, modURL }) => [
+              switcherIcon: vue.withCtx(({ expanded }) => [
+                expanded ? (vue.openBlock(), vue.createElementBlock("i", _hoisted_4)) : (vue.openBlock(), vue.createElementBlock("i", _hoisted_5))
+              ]),
+              title: vue.withCtx(({ title, key, modPic, modURL, modCount, modPackCount }) => [
                 modURL ? (vue.openBlock(), vue.createElementBlock("a", {
                   key: 0,
                   target: "_blank",
                   href: modURL,
                   title
                 }, [
-                  vue.createElementVNode("div", _hoisted_4, [
+                  vue.createElementVNode("div", _hoisted_7, [
                     modPic ? (vue.openBlock(), vue.createElementBlock("img", {
                       key: 0,
-                      class: "cover",
+                      class: "mm_fav_cover",
                       src: modPic,
                       alt: title
-                    }, null, 8, _hoisted_5)) : vue.createCommentVNode("", true),
-                    vue.createElementVNode("span", _hoisted_6, vue.toDisplayString(title), 1)
+                    }, null, 8, _hoisted_8)) : vue.createCommentVNode("", true),
+                    vue.createElementVNode("span", _hoisted_9, vue.toDisplayString(title), 1)
                   ])
-                ], 8, _hoisted_3)) : (vue.openBlock(), vue.createElementBlock(vue.Fragment, { key: 1 }, [
-                  vue.createTextVNode(vue.toDisplayString(title), 1)
+                ], 8, _hoisted_6)) : (vue.openBlock(), vue.createElementBlock(vue.Fragment, { key: 1 }, [
+                  vue.createElementVNode("span", null, vue.toDisplayString(title), 1),
+                  modCount ? (vue.openBlock(), vue.createElementBlock("span", _hoisted_10, [
+                    _cache[5] || (_cache[5] = vue.createElementVNode("i", { class: "fa fa-cubes" }, null, -1)),
+                    vue.createTextVNode(" x" + vue.toDisplayString(modCount), 1)
+                  ])) : vue.createCommentVNode("", true),
+                  modPackCount ? (vue.openBlock(), vue.createElementBlock("span", _hoisted_11, [
+                    _cache[6] || (_cache[6] = vue.createElementVNode("i", { class: "fa fa-file-zip-o" }, null, -1)),
+                    vue.createTextVNode(" x" + vue.toDisplayString(modPackCount), 1)
+                  ])) : vue.createCommentVNode("", true),
+                  modCount === 0 && modPackCount === 0 ? (vue.openBlock(), vue.createElementBlock("span", _hoisted_12, "空收藏夹")) : vue.createCommentVNode("", true)
                 ], 64))
               ]),
               _: 1
             }, 8, ["expandedKeys", "selectedKeys", "tree-data"])
-          ])
-        ]);
+          ])) : vue.createCommentVNode("", true)
+        ], 64);
       };
     }
   });
@@ -250,16 +325,40 @@
     }
     return target;
   };
-  const App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-8268fb43"]]);
+  const App = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-22dd0357"]]);
   const app = vue.createApp(App);
   const pinia = pinia$1.createPinia();
   app.use(pinia);
+  app.use(antDesignVue.Button);
   app.use(antDesignVue.Tree);
+  const appScroll = (nodeDiv) => {
+    const node = document.querySelector(".center .main .news_block");
+    if (node) {
+      let top = node.getBoundingClientRect().top;
+      let _top = 0;
+      switch (true) {
+        case (top >= 60 && top <= 270):
+          _top = top;
+          break;
+        case top < 60:
+          _top = 70;
+          break;
+        case top > 270:
+          _top = 286;
+          break;
+      }
+      nodeDiv.style.top = `${_top}px`;
+    }
+  };
   app.mount(
     (() => {
       const nodeDiv = document.createElement("div");
       nodeDiv.id = "app";
       document.body.append(nodeDiv);
+      appScroll(nodeDiv);
+      window.addEventListener("scroll", function() {
+        appScroll(nodeDiv);
+      });
       return nodeDiv;
     })()
   );
